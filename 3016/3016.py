72445f3e31f55afca001e51147c5037f85dabdca
rahul-challa <challaviswanadhrahul@gmail.com>
2024-08-06 12:27:37 +0530

    06-08-2024

---
+++ 3016/3016.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        counts = [0] * 26
+        for c in word:
+            counts[ord(c) - ord("a")] += 1
+        
+        counts.sort(reverse = True)
+
+        res = 0
+        distinct = 0
+        for cnt in counts:
+            if cnt > 0:   
+                res += cnt * (1 + distinct // 8)
+                distinct += 1
+        return res
