fecf4038d2f5da3d5122856dedb419325c3a1e42
rahul-challa <challaviswanadhrahul@gmail.com>
2025-07-13 19:30:57 -0700

    13th July 2025

--- Solutions/2678/2678.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countSeniors(self, details: List[str]) -> int:
-        res = 0
-        for i in details:
-            if int(i[11:13]) > 60:
-                res += 1
-        
-        return res
